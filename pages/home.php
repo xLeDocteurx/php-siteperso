c6071b7709fc64ce4a8c7583ed893f757db69785
xLeDocteurx <lenoirc.fr@gmail.com>
2018-07-29 17:43:46 +0200

    Did the general layout of the resume part. I'll add the content... one day . And make responsive// usable the portfolio. for now it's just magic

--- pages/home.php
+++ pages/home.php
@@ -1,18 +1,96 @@
-<div class="row py-5">
+<div class="row pb-5">
 
 
 
+    <div id="portfolio" class="jumbotron mb-0 text-center">
+        <!-- <h1>Portfolio</h1> -->
+        <p>
+            <img class="img-fluid" src="images/001.png" alt="">
+        </p>
+        <a class="btn btn-outline-danger" href="#quisuisje">Qui suis-je ?</a>
+    </div> 
+
+    <div id="quisuisje" class="jumbotron mb-0 text-center bg-light">
+        <hr>
+        <h1>Développeur web</h1>
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
+                <hr>
+                Vous recherchez un développeur Front-end confirmé, 
+                passionné par la création d'applications mobiles hybrides multiplateformes (code non-natif), 
+                le responsive design et le SEO, alors ma candidature devrait vous intéresser !
+                <hr>
+                Maitrisant parfaitement l’ensemble des étapes de la création Web, 
+                tant d’un point de vue technique qu’éditorial, 
+                je suis en mesure de prendre en charge la création web en responsive mais aussi 
+                la maintenance quotidienne de sites existants en assurant la mise à jour de contenus 
+                graphiques et éditoriaux tout en veillant à l’optimisation du référencement naturel mais aussi à 
+                l’ergonomie des interfaces (UX).
+                <hr>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+        <a class="btn btn-outline-danger" href="#competences" >Compétences</a>
+    </div> 
+
+    <div id="competences" class="jumbotron mb-0 text-center">
+        <hr>
+        <h1>Mes compétences</h1>
+        <hr>
+        <div class="row"> 
+                <div class="col-md">
+                        <div class="card border-light mb-3">
+                <div class="card-header"><i class="fab fa-node fa-3"></i></div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md">
+                        <div class="card border-light mb-3">
+                <div class="card-header"><i class="fab fa-php fa-3"></i></div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+    
+            <div class="col-md">
+                <div class="card border-light mb-3">
+                <div class="card-header"><i class="fas fa-database fa-3"></i></div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+            </div>
+            
+            <div class="col-md">
+                <div class="card border-light mb-3">
+                <div class="card-header"><i class="fas fa-gamepad fa-3"></i></div>
+                <div class="card-body">
+                    <h5 class="card-title">Light card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <a class="btn btn-outline-danger" href="#else" >Else</a>
+    </div> 
 
 
 
     <div class="col-md-2"></div>
-    <div class="col-md-8">
+    <div id="else" class="col-md-8">
+    <hr>
         <h1>Home</h1>
         <!-- <h1>maison sucré maison</h1> -->
         <hr>
-
-
-
         <?php
         $datas = getUserData();
 
