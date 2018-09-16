9e25bd54a1aef42cc7dba03dee62f44a3ab0f28f
xLeDocteurx <lenoirc.fr@gmail.com>
2018-09-16 20:58:48 +0200

    big update

--- parts/head.php
+++ parts/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">  
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Lenoir Christopher - A Far Worst CV</title>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
     <link rel="stylesheet" href="bootstrap/bootstrap.css">    
